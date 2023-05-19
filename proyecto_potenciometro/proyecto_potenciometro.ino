8cdb5cebc5e5b3df3a9ccc994738169c8f2eca13
julianCuellarP <juliancuellarp@gmail.com>
2023-05-19 10:46:54 -0500

    pin para el microbit

--- proyecto_potenciometro/proyecto_potenciometro.ino
+++ proyecto_potenciometro/proyecto_potenciometro.ino
@@ -1,10 +1,10 @@
-static int canon1 = 5;
-static int canon2 = 6;
-static int botonF = 2;
-static int potent_1 = 0;
-static int potent_2 = 1;
-static int max = 1023;
-static int min = 21;
+int canon1 = 5;
+int canon2 = 6;
+int botonF = 2;
+int potent_1 = 0;
+int potent_2 = 1;
+int max = 1023;
+int min = 21;
 
 
 int V_1 = 0; 
@@ -24,6 +24,7 @@ void setup() {
 pinMode(botonF,INPUT); 
 pinMode(canon1,OUTPUT);
 pinMode(canon2,OUTPUT);
+pinMode(13,OUTPUT);  
 motor1.attach(canon1);
 motor2.attach(canon2);
 motor1.write(0);
@@ -32,6 +33,7 @@ Serial.begin(9600);
 }
 
 void loop() {
+digitalWrite(13,LOW);
 V_1 = analogRead(potent_1);
 A_1 = analogRead(potent_2); 
 velocidad = map(V_1,min,max,0,999);
@@ -39,6 +41,7 @@ angulo = map(A_1,min,max,-45,50);
 angulo_f1 = map(A_1,min,max,0,90);
 angulo_f2 = map(A_1,min,max,180,90);
 if(digitalRead(botonF) == HIGH){
+  digitalWrite(13,HIGH);
   Serial.print(angulo);
   Serial.print(",");
   Serial.println(velocidad);
@@ -52,6 +55,8 @@ if(digitalRead(botonF) == HIGH){
     turno++;
   }
  
+}
+
 }
 
 }
